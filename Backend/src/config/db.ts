8901cfd29641fa8a371bdd063d6636526368780e
Yeu <yeumensilva9@gmail.com>
2026-03-07 13:46:09 -0300

    Update variable name

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.HOST || "", {
+        await mongoose.connect(process.env.MONGO_URI || "", {
             serverSelectionTimeoutMS: 5000, 
             connectTimeoutMS: 10000,
         })
