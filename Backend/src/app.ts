21d0db11d5fd767b708d8a28d68e7d8f7c143d84
Yeu <yeumensilva9@gmail.com>
2026-03-08 17:04:16 -0300

    Use cors in the back

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import notesRoutes from './routes/notesRoutes.js'
 import rateLimiter from './middleware/rateLimiter.js';
 
 
 const app = express();
+app.use(cors({
+  origin: process.env.FRONT_ORIGIN, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.set('trust proxy', true);
 app.use(express.json())
 app.use(rateLimiter)
