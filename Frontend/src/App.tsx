37a64c0966c70c587653208a9cf806821498828c
Yeu <yeumensilva9@gmail.com>
2026-03-08 13:39:04 -0300

    Create Routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import NoteDetailPage from "./pages/NoteDetailPage"
+
 
 function App() {
   return (
     <div>
-      
+        
+        <Routes>
+            <Route path="/" element={<HomePage/>}></Route>
+            <Route path="/create" element={<CreatePage/>}></Route>
+            <Route path="/note/:id" element={<NoteDetailPage/>}></Route>
+        </Routes>
     </div>
   )
 }
